dc798372e6507b9139022098867f3594d57a0f44
Weeleeam <william.jonnatan@ui.ac.id>
2026-02-12 21:37:58 +0700

    Add arithmetic-multiply unit test

---
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -0,0 +1,27 @@
+package com.example.individualprep.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArithmeticUtilityTest {
+
+    @Test
+    void multiply_shouldReturnCorrectResult_positive() {
+        ArithmeticUtility util = new ArithmeticUtility();
+        double a = 9;
+        double b = 5;
+        double expected = 45.0;
+
+        assertEquals(expected, util.multiply(a,b));
+    }
+
+    @Test
+    void multiply_shouldReturnCorrectResult_negative() {
+        ArithmeticUtility util = new ArithmeticUtility();
+        double a = 7;
+        double b = -6;
+        double expected = -42.0;
+
+        assertEquals(expected, util.multiply(a,b));
+    }
+}
