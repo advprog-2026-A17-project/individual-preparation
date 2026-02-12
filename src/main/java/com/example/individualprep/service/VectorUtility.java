58f39747cf1207ca8e3f15920bdc305fe3b5debc
Jovanus Irwan <jovanusirwan@gmail.com>
2026-02-12 17:50:52 +0700

    vector utility belum unit test

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -11,8 +11,13 @@ public class VectorUtility {
     }
 
     public double[] subtract(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+
+        int size= v1.length;
+        double[] result= new double[size];
+        for (int i=0; i<size; i++) {
+            result[i]= v1[i]- v2[i];
+        }
+        return result;
     }
 
     public double[] multiply(double[] v1, int x) {
