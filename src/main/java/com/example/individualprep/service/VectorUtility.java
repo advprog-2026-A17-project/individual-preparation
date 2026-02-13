e74a1b47b4b56f246770375f1c8503f3ae22ebf4
Whosmomo <pradiptaadinugroho@gmail.com>
2026-02-13 13:39:36 +0700

    implement dotproduct vector

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -32,8 +32,15 @@ public class VectorUtility {
     }
     
     public double dotProduct(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        validateNotNull(v1, v2);
+        validateSameSize(v1, v2);
+
+        double result = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            result += v1[i] * v2[i];
+        }
+
+        return result;
     }
     
     public double norm(double[] v1) {
