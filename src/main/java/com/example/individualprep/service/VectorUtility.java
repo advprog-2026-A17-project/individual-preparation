e836a540cc1ad301e4d5b20f8a5d0f3c8ffac0d9
Weeleeam <william.jonnatan@ui.ac.id>
2026-02-13 20:42:52 +0700

    Fix unit test

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -22,8 +22,14 @@ public class VectorUtility {
     }
 
     public double[] subtract(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        validateNotNull(v1, v2);
+        validateSameSize(v1, v2);
+        int size= v1.length;
+        double[] result= new double[size];
+        for (int i=0; i<size; i++) {
+            result[i]= v1[i]- v2[i];
+        }
+        return result;
     }
 
     public double[] multiply(double[] v1, int x) {
--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -68,4 +68,40 @@ class VectorUtilityTest {
                 vectorUtility.add(v1, v2)
         );
     }
+    @Test
+    void subtract_shouldReturnCorrectResult() {
+        VectorUtility vectorUtility = new VectorUtility();
+
+        double[] v1 = {10, 8, 6};
+        double[] v2 = {1, 2, 3};
+
+        double[] expected = new double[]{9, 6, 3};
+        double[] result = vectorUtility.subtract(v1, v2);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void subtract_shouldThrowException_whenVectorNull() {
+        VectorUtility vectorUtility = new VectorUtility();
+
+        double[] v1 = {};
+        double[] v2 = {3, 1, 2};
+
+        assertThrows(IllegalArgumentException.class, () ->
+                vectorUtility.subtract(v1, v2)
+        );
+    }
+
+    @Test
+    void subtract_shouldThrowException_whenDifferentLength() {
+        VectorUtility vectorUtility = new VectorUtility();
+
+        double[] v1 = {5, 6};
+        double[] v2 = {3, 1, 2};
+
+        assertThrows(IllegalArgumentException.class, () ->
+                vectorUtility.subtract(v1, v2)
+        );
+    }
 }
